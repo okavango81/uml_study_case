d79c079e70e35689342765c31966e4d982a29b9b
okavango81 <eguilhermeleitedev81@gmail.com>
2024-03-15 08:46:29 -0300

    project created

---
+++ src/main/java/com/okavango/uml_study_case/UmlStudyCaseApplication.java
@@ -0,0 +1,13 @@
+package com.okavango.uml_study_case;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlStudyCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmlStudyCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/okavango/uml_study_case/UmlStudyCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okavango.uml_study_case;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmlStudyCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
